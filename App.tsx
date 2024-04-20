8be112906dc67a9a2f558c0452fbd7b962202a4b
halli21 <haraldurf21@ru.is>
2024-04-20 21:07:18 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-black h-full flex justify-center">
+      <Text className="text-blue-600 self-center">
+        This is styled with tailwind
+      </Text>
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Run
+```
+npm install
+
+npx expo
+```
