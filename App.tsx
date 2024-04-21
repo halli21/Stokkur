bb4d8333c06253d40b2f8a6c23989237095e7051
halli21 <haraldurf21@ru.is>
2024-04-21 14:08:40 +0000

    draw card

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import { Card } from "./components/card";
 import { Deck } from "./utils/deck";
+import { useState } from "react";
 
 export default function App() {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const handleDraw = () => {
+    const newIndex = Math.floor(Math.random() * 52);
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <View className="h-full flex justify-center items-center">
-      <Text className="text-blue-600">This is styled with tailwind</Text>
-      <Card card={Deck[0]} />
+      <Card card={Deck[currentIndex]} />
+
+      <TouchableOpacity
+        className="bg-custom-blue
+         mt-5 px-10 py-5 rounded-lg"
+        onPress={handleDraw}
+      >
+        <Text className="text-white">Draw</Text>
+      </TouchableOpacity>
     </View>
   );
 }
