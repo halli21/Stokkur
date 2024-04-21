348035a7424aa5ce94fa2b47c971e341cb6d4041
halli21 <haraldurf21@ru.is>
2024-04-21 13:30:44 +0000

    created a deck with required images

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "./components/card";
+import { Deck } from "./utils/deck";
 
 export default function App() {
   return (
     <View className="h-full flex justify-center items-center">
       <Text className="text-blue-600">This is styled with tailwind</Text>
-      <Card rank="A" suit="H" />
+      <Card card={Deck[0]} />
     </View>
   );
 }
---
+++ utils/deck.ts
@@ -0,0 +1,88 @@
+import { CardType } from "../type/card";
+
+export const Deck: CardType[] = [
+  {
+    id: "AC",
+    rank: 14,
+    suit: "C",
+    src: require("../assets/cardImages/AC.png"),
+  },
+  {
+    id: "AD",
+    rank: 14,
+    suit: "D",
+    src: require("../assets/cardImages/AD.png"),
+  },
+  {
+    id: "AH",
+    rank: 14,
+    suit: "H",
+    src: require("../assets/cardImages/AH.png"),
+  },
+  {
+    id: "AS",
+    rank: 14,
+    suit: "S",
+    src: require("../assets/cardImages/AS.png"),
+  },
+];
+
+// { id: 'KC', rank: 13, suit: 'C' },
+// { id: 'KD', rank: 13, suit: 'D' },
+// { id: 'KH', rank: 13, suit: 'H' },
+// { id: 'KS', rank: 13, suit: 'S' },
+
+// { id: 'QC', rank: 12, suit: 'C' },
+// { id: 'QD', rank: 12, suit: 'D' },
+// { id: 'QH', rank: 12, suit: 'H' },
+// { id: 'QS', rank: 12, suit: 'S' },
+
+// { id: 'JC', rank: 11, suit: 'C' },
+// { id: 'JD', rank: 11, suit: 'D' },
+// { id: 'JH', rank: 11, suit: 'H' },
+// { id: 'JS', rank: 11, suit: 'S' },
+
+// { id: '10C', rank: 10, suit: 'C' },
+// { id: '10D', rank: 10, suit: 'D' },
+// { id: '10H', rank: 10, suit: 'H' },
+// { id: '10S', rank: 10, suit: 'S' },
+
+// { id: '9C', rank: 9, suit: 'C' },
+// { id: '9D', rank: 9, suit: 'D' },
+// { id: '9H', rank: 9, suit: 'H' },
+// { id: '9S', rank: 9, suit: 'S' },
+
+// { id: '8C', rank: 8, suit: 'C' },
+// { id: '8D', rank: 8, suit: 'D' },
+// { id: '8H', rank: 8, suit: 'H' },
+// { id: '8S', rank: 8, suit: 'S' },
+
+// { id: '7C', rank: 7, suit: 'C'  },
+// { id: '7D', rank: 7, suit: 'D'  },
+// { id: '7H', rank: 7, suit: 'H'  },
+// { id: '7S', rank: 7, suit: 'S'  },
+
+// { id: '6C', rank: 6, suit: 'C' },
+// { id: '6D', rank: 6, suit: 'D' },
+// { id: '6H', rank: 6, suit: 'H' },
+// { id: '6S', rank: 6, suit: 'S' },
+
+// { id: '5C', rank: 5, suit: 'C' },
+// { id: '5D', rank: 5, suit: 'D' },
+// { id: '5H', rank: 5, suit: 'H' },
+// { id: '5S', rank: 5, suit: 'S' },
+
+// { id: '4C', rank: 4, suit: 'C' },
+// { id: '4D', rank: 4, suit: 'D' },
+// { id: '4H', rank: 4, suit: 'H' },
+// { id: '4S', rank: 4, suit: 'S' },
+
+// { id: '3C', rank: 3, suit: 'C' },
+// { id: '3D', rank: 3, suit: 'D' },
+// { id: '3H', rank: 3, suit: 'H' },
+// { id: '3S', rank: 3, suit: 'S' },
+
+// { id: '2C', rank: 2, suit: 'C' },
+// { id: '2D', rank: 2, suit: 'D' },
+// { id: '2H', rank: 2, suit: 'H' },
+// { id: '2S', rank: 2, suit: 'S' },
