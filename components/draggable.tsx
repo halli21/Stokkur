5fbfd10b177bda3234ef3b920602705f050a9a5d
halli21 <haraldurf21@ru.is>
2024-04-22 12:01:21 +0000

    draggable with spring back

--- components/draggable.tsx
+++ components/draggable.tsx
@@ -1,21 +1,51 @@
-import { PanGestureHandler } from "react-native-gesture-handler";
-import Animated, {
-  useAnimatedGestureHandler,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Animated,
+  PanResponder,
+  StyleSheet,
+  UIManager,
+  View,
+  findNodeHandle,
+} from "react-native";
 
 interface DraggableProps {
   children: React.ReactNode;
 }
 
-export const Draggable = ({ children }: DraggableProps) => {
+export const DraggableComponent = ({ children }: DraggableProps) => {
+  const componentRef = useRef(null);
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+        return gestureState.dx != 0 && gestureState.dy != 0;
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: (_, gesture) => {
+        const handle = findNodeHandle(componentRef.current);
+        if (handle) {
+          UIManager.measure(handle, (x, y, width, height, pageX, pageY) => {
+            Animated.spring(pan, {
+              toValue: { x: 0, y: 0 },
+              useNativeDriver: false,
+            }).start();
+          });
+        }
+      },
+    })
+  ).current;
+
   return (
-    <Animated.View>
-      <PanGestureHandler>
-        <Animated.View>{children}</Animated.View>
-      </PanGestureHandler>
+    <Animated.View
+      style={pan.getLayout()}
+      ref={componentRef}
+      {...panResponder.panHandlers}
+    >
+      {children}
     </Animated.View>
   );
 };
--- components/player_hand.tsx
+++ components/player_hand.tsx
@@ -1,7 +1,7 @@
 import { View, FlatList } from "react-native";
 import { CardType } from "../type/card";
 import { Card } from "./card";
-import { Draggable } from "./draggable";
+import { DraggableComponent } from "./draggable";
 
 interface PlayerHandProps {
   hand: CardType[];
@@ -9,18 +9,23 @@ interface PlayerHandProps {
 
 export const PlayerHand = ({ hand }: PlayerHandProps) => {
   return (
-    <FlatList
-      className="mt-64"
-      horizontal
-      data={hand}
-      renderItem={({ item, index }) => (
-        <View className="mx-1">
-          <Card key={item.id} card={item} index={index} />
-        </View>
-      )}
-      keyExtractor={(item) => item.id}
-      showsHorizontalScrollIndicator={false}
-    />
+    <View>
+     
+      <FlatList
+        className="mt-64"
+        horizontal
+        data={hand}
+        renderItem={({ item, index }) => (
+          <View className="mx-1">
+            <DraggableComponent>
+              <Card key={item.id} card={item} index={index} />
+            </DraggableComponent>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
   );
 };
 
